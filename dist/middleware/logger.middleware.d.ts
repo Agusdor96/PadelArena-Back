7d66da3400bbe87271ab003054df9a3487ff8022
agusdor96 <miroetto12@gmail.com>
2024-08-20 19:41:38 -0300

    Revert "add folder config with file typeorm.ts- config typeorm in app.module"
    This reverts commit 96ae59d5f7dd92ac8b72b21078fd4f7c4d301e8c.

--- dist/middleware/logger.middleware.d.ts
+++
@@ -1,2 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-export declare function loggerGlobal(req: Request, res: Response, next: NextFunction): void;
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import typeorm from './config/typeorm';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal:true,
-      envFilePath: "./.env.development",
-      load:[typeorm]
-    }),
-    TypeOrmModule.forRootAsync({
-      inject:[ConfigService],
-      useFactory: (configService:ConfigService)=>
-        configService.get("typeorm"),
-    })
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
