0f6305f3666eade40e09fafb5cc0c65b02794562
agusdor96 <miroetto12@gmail.com>
2024-08-21 14:35:27 -0300

    add many to many relation with match and import v4 uuid

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -1,19 +1,23 @@
 import { Team } from "src/team/entities/team.entity";
 import { Column, Entity, JoinColumn, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import {v4 as uuid} from 'uuid'
 
 @Entity({
     name: "CATEGORY"
 })
 export class Category {
     @PrimaryGeneratedColumn("uuid")
-    id:string
+        id:string = uuid()
 
     @Column({type: "varchar", length: 50, nullable:false})
-    name:string
+        name:string
     
     @Column({type: "text", nullable:false})
-    description:string
+        description:string
 
     @OneToMany(() => Team, (team) => team.category)
-    team: Team
+        team: Team
+
+    @OneToMany(() => Tournament, (tournaments)=> tournaments.category)
+        tournaments: Tournament
 }
--- src/team/entities/team.entity.ts
+++ src/team/entities/team.entity.ts
@@ -1,6 +1,7 @@
 import { Category } from "src/category/entities/category.entity";
 import { User } from "src/user/entities/user.entity";
 import { Column, Entity, JoinTable, ManyToMany, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import {v4 as uuid} from 'uuid'
 
 @Entity({
     name: "TEAM"
@@ -8,7 +9,7 @@ import { Column, Entity, JoinTable, ManyToMany, ManyToOne, PrimaryGeneratedColum
 
 export class Team {
     @PrimaryGeneratedColumn("uuid")
-        id:string;
+        id:string = uuid()
 
     @Column({type: "varchar", length: 50, nullable:false})
         name: string
@@ -21,5 +22,9 @@ export class Team {
         user: User[]
 
     @ManyToOne(() => Tournament, (tournament) => tournament.team)
-        tournament:Tournament;
+        tournament:Tournament
+    
+    @ManyToMany(()=> Match, (match)=> match.teams)
+    @JoinTable({name: "TEAM_MATCH"})
+        match: Match
 }
