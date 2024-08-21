52663787f29eed71a3c705b9bee129c527c95b0f
Anacasconi1 <sabriicasconii@gmail.com>
2024-08-21 19:29:26 -0300

    ADD/UPDATE files funcionality

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -5,7 +5,6 @@ import { User } from 'src/user/entities/user.entity';
 import { Repository } from 'typeorm';
 import { FileRepository } from './file.repository';
 
-
 @Injectable()
 export class FileService {
   constructor(
@@ -13,21 +12,53 @@ export class FileService {
     private tournamentRepostory: Repository<Tournament>,
     @InjectRepository(User)
     private userRepository: Repository<User>,
-    private fileRepository: FileRepository
-  ){}
-  async UpdateTournamentPrincipalImage(id:string, file: Express.Multer.File){
-    const tournament = await this.tournamentRepostory.findOne({where: {id}})
-    if(!tournament){
-      throw new NotFoundException('No fue posible encontrar el torneo')
-    }else {
-      const tournamentFlayer = (await this.fileRepository.uploadImage(file)).secure_url;
-      await this.tournamentRepostory.update(id, {})
+    private fileRepository: FileRepository,
+  ) {}
+  async UpdateTournamentPrincipalImage(id: string, file: Express.Multer.File) {
+    const tournament = await this.tournamentRepostory.findOne({
+      where: { id },
+    });
+    if (!tournament) {
+      throw new NotFoundException('No fue posible encontrar el torneo');
+    } else {
+      const tournamentFlyer = (await this.fileRepository.uploadImage(file))
+        .secure_url;
+      await this.tournamentRepostory.update(id, { tournamentFlyer });
+      return { message: 'Imagen actualizada con exito con exito' };
     }
   }
 
-  UploadTournamentMultimedia(){}
+  async UploadTournamentMultimedia(id: string, file: Express.Multer.File) {
+    const tournament = await this.tournamentRepostory.findOne({
+      where: { id },
+    });
+    if (!tournament) {
+      throw new NotFoundException('No fue posible encontrar el torneo');
+    } else {
+      const tournamentNewImg = (await this.fileRepository.uploadImage(file))
+        .secure_url;
+      const imgArray = tournament.imgUrl
+      const arrayUpdated = {tournamentNewImg, ...imgArray}
+      const tournamentUpdated:Tournament = {
+        imgUrl: arrayUpdated,
+        ...tournament
+      }
+      await this.tournamentRepostory.save(tournamentUpdated);
+      return { message: 'Imagen subida con exito con exito' };
+    }
+  }
 
-  UpdateProfileImage(){}
-
-  
+  async UpdateProfileImage(id:string, file: Express.Multer.File) {
+    const user = await this.userRepository.findOne({
+      where: { id },
+    });
+    if (!user) {
+      throw new NotFoundException('No fue posible encontrar al usuario');
+    } else {
+      const profilePhoto = (await this.fileRepository.uploadImage(file))
+        .secure_url;
+      await this.userRepository.update(id, { profileImg: profilePhoto });
+      return { message: 'Foto de perfil actualizada con exito con exito' };
+    }
+  }
 }
