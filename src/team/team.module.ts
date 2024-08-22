0b618636606ae653076f24b1bb5b2ef8fd9e475f
Anacasconi1 <sabriicasconii@gmail.com>
2024-08-22 16:56:14 -0300

    UPDATE create Team method

--- src/team/team.module.ts
+++ src/team/team.module.ts
@@ -6,9 +6,10 @@ import { Team } from './entities/team.entity';
 import { TournamentService } from 'src/tournament/tournament.service';
 import { Tournament } from 'src/tournament/entities/tournament.entity';
 import { Category } from 'src/category/entities/category.entity';
+import { User } from 'src/user/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Team, Tournament, Category])],
+  imports: [TypeOrmModule.forFeature([Team, Tournament, Category, User])],
   controllers: [TeamController],
   providers: [TeamService, TournamentService],
 })
--- src/team/team.service.ts
+++ src/team/team.service.ts
@@ -1,10 +1,15 @@
-import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { TeamDto } from './dto/team.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Team } from './entities/team.entity';
 import { Repository } from 'typeorm';
 import { TournamentService } from 'src/tournament/tournament.service';
-
+import { User } from 'src/user/entities/user.entity';
 
 @Injectable()
 export class TeamService {
@@ -12,40 +17,54 @@ export class TeamService {
     @InjectRepository(Team)
     private teamRepository: Repository<Team>,
     @Inject()
-    private tournamentService: TournamentService
-  ){}
-  async newTeam(tournamentId:string ,TeamDto: TeamDto) {
-    const tournament = await this.tournamentService.getTournament(tournamentId)
-    if(!tournament){
-      throw new NotFoundException('El equipo no existe, revisa la informacion proporcionada')
-    }else {
-      const team = {
-        name: TeamDto.name,
-        category: tournament.category,
-        user: TeamDto.players,
-        tournament: tournament
+    private tournamentService: TournamentService,
+    @InjectRepository(User)
+    private userService: Repository<User>,
+  ) {}
+  async newTeam(tournamentId: string, TeamDto: TeamDto) {
+    const tournament = await this.tournamentService.getTournament(tournamentId);
+    for (const user of TeamDto.players) {
+      const users = await this.userService.findOne({ where: { id: user.id } });
+      const userHaveTeam = users.team;
+      if (userHaveTeam) {
+        throw new BadRequestException(
+          'El usuario solo puede pertenecer a un equipo',
+        );
+      } else {
+        const team = {
+          name: TeamDto.name,
+          category: tournament.category,
+          user: TeamDto.players,
+          tournament: tournament,
+        };
+        await this.teamRepository.save(team);
+        return { message: 'Equipo creado con exito', team };
       }
-      await this.teamRepository.save(team)
-      return {message: 'Equipo creado con exito', team}
     }
   }
 
   async findOneTeam(id: string) {
-    const team = await this.teamRepository.findOne({where: {id}, relations: {category:true}})
-    if(!team){
-      throw new NotFoundException('El equipo no existe, revisa la informacion proporcionada')
-    }else {
-      return team
+    const team = await this.teamRepository.findOne({
+      where: { id },
+      relations: { category: true },
+    });
+    if (!team) {
+      throw new NotFoundException(
+        'El equipo no existe, revisa la informacion proporcionada',
+      );
+    } else {
+      return team;
     }
   }
 
   async findAllTeamsByTournament(tournamentId: string) {
-    const tournament = await this.tournamentService.getTournament(tournamentId)
-    if (!tournament.team.length){
-      throw new NotFoundException('No se encontraron equipos inscriptos en este torneo.')
-    }
-    else {
-      return tournament.team
+    const tournament = await this.tournamentService.getTournament(tournamentId);
+    if (!tournament.team.length) {
+      throw new NotFoundException(
+        'No se encontraron equipos inscriptos en este torneo.',
+      );
+    } else {
+      return tournament.team;
     }
   }
 }
