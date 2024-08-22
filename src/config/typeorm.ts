6f0c5e8cd01ef1e2416fc20458c39d1ed97592fd
Esteban Necuse <estebannecuse@gmail.com>
2024-08-21 21:03:34 -0300

    precarga de categorias y dto ok

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -14,7 +14,7 @@ const config = {
   entities: ['dist/**/*.entity{.ts,.js}'],
   logging: false,
   synchronize: true,
-  dropSchema: true,
+  dropSchema: false,
 };
 
 export default registerAs('typeorm', () => config);
