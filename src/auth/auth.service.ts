631d1c60ecc88daa4758b7785ad7b2ac56669953
agusdor96 <miroetto12@gmail.com>
2024-08-27 19:47:49 -0300

    ADD return to authGoogleSign if userFromDb doesnt exist

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as bcrypt from 'bcrypt'
@@ -84,9 +84,10 @@ export class AuthService {
 
       return {message: 'Inicio de sesion realizado con exito', token, userClean}
     } else if(!userFromDb){
-      
+      return {
+        googleUser,
+        message: "Usuario no registrado. Se necesitan mas datos para completar el proceso"
+      }
     }
-
-
   }
 }
