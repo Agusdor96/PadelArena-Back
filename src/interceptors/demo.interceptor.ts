6c6881e57addd328c2e7e34e1dd11ab5c1593f72
Anacasconi1 <sabriicasconii@gmail.com>
2024-08-29 19:42:05 -0300

    ADD some changes at mp module

--- src/interceptors/demo.interceptor.ts
+++ src/interceptors/demo.interceptor.ts
@@ -4,7 +4,7 @@ import { Observable } from "rxjs";
 @Injectable()
 export class DEMO implements NestInterceptor{
 intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
-    console.log(context.switchToHttp().getRequest);
+    console.log(context.switchToHttp().getRequest());
     
     return next.handle().pipe(
         
