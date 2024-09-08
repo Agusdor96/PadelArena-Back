18b2973cdd373b67fa23c5d9e382cf7825e361e3
Esteban Necuse <estebannecuse@gmail.com>
2024-09-08 16:26:09 -0300

    "mp y player estats auth-controllers"

--- src/mercado-pago/mercado-pago.controller.ts
+++ src/mercado-pago/mercado-pago.controller.ts
@@ -1,34 +1,47 @@
-import { Controller, Post, Body, UseInterceptors, Get, Param, ParseUUIDPipe, HttpCode, Query } from '@nestjs/common';
+import { Controller, Post, Body, UseInterceptors, Get, Param, ParseUUIDPipe, HttpCode, Query, UseGuards } from '@nestjs/common';
 import { MercadoPagoService } from './mercado-pago.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { dataPaymentDto } from './dtos/dataPayment.dto';
 import { HeaderInterceptor } from 'src/interceptors/demo.interceptor';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { RolesGuard } from 'src/guards/roles.guard';
+import { Roles } from 'src/decorators/roles.decorator';
+import { RoleEnum } from 'src/user/roles.enum';
 
 @ApiTags("MERCADO-PAGO")
 @Controller('mercado-pago')
 export class MercadoPagoController {
   constructor(private readonly mercadoPagoService: MercadoPagoService) {}
 
+  @ApiBearerAuth()
   @Post('create_preference')
+  @UseGuards(AuthGuard)
   apiMPConnection(@Body() req: dataPaymentDto){
     return this.mercadoPagoService.mpConnections(req)
   }
-
+  @ApiBearerAuth()
   @HttpCode(201)
   @UseInterceptors(HeaderInterceptor)
   @Post('feedback')
+  @UseGuards(AuthGuard)
   feedbackPayment (@Query('data.id') id: string, @Body() body : string){
     this.mercadoPagoService.encryptHeaders(body)
     this.mercadoPagoService.getpayment(id)
     return id
   }
 
+  @ApiBearerAuth()
+  @Roles(RoleEnum.ADMIN)
   @Get('allPayments')
+  @UseGuards(AuthGuard,RolesGuard)
   getAllPayments(){
     return this.mercadoPagoService.getPreferenceByUserId()
   }
 
+  @ApiBearerAuth()
+  @Roles(RoleEnum.ADMIN)
   @Get("byTournament/:tournamentId")
+  @UseGuards(AuthGuard,RolesGuard)
   allTournamentPayments(@Param("tournamentId", ParseUUIDPipe) tournamentId:string){
     try{
       return this.mercadoPagoService.getPaymentsFromTournament(tournamentId)
@@ -36,7 +49,9 @@ export class MercadoPagoController {
       throw err
     }
   }
+  @ApiBearerAuth() 
   @Get("byUser/:userId")
+  @UseGuards(AuthGuard)
   allUserPayments(@Param("userId", ParseUUIDPipe) userId:string){
     try{
       return this.mercadoPagoService.getPaymentsFromUser(userId)
