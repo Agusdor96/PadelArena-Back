46fc078259a211f841bfe606718ee47448e6f3f2
agusdor96 <miroetto12@gmail.com>
2024-09-09 00:58:32 -0300

    FIX bugs, exception Filter, userIdInterceptor

--- src/interceptors/userId.interceptor.ts
+++ src/interceptors/userId.interceptor.ts
@@ -7,7 +7,15 @@ export class UserIdINterceptor implements NestInterceptor{
     constructor(private readonly jwtService: JwtService) {}
     intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> {
         const request = context.switchToHttp().getRequest();
+        if(!request.headers["authorization"]){
+            throw new BadRequestException ("Falta la autorizacion en el header de la request")
+          }  
+
         const token = request.headers.authorization.split(" ")[1];
+        if(!token){
+            throw new BadRequestException("No se encontro el Bearer token")
+        }
+        
         const paramsId = request.params.userId;
         const decriptedToken = this.jwtService.decode(token);
        
