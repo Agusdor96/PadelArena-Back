9d678b30a49076402000bab27090866321e58bb8
agusdor96 <miroetto12@gmail.com>
2024-09-29 12:31:27 -0300

    Add custom for stadistics controller

--- src/player-stadistics/player-stadistics.controller.ts
+++ src/player-stadistics/player-stadistics.controller.ts
@@ -1,20 +1,16 @@
-import { Controller, Get, Param, ParseUUIDPipe, UseGuards} from '@nestjs/common';
+import { Controller, Get, Param, ParseUUIDPipe} from '@nestjs/common';
 import { PlayerStadisticsService } from './player-stadistics.service';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { AuthGuard } from '../guards/auth.guard';
-import { SwaggerGetStadistics } from '../decorators/SwaggerDecorators/Stadistics.decorator';
+import { ApiTags } from '@nestjs/swagger';
+import { CustomGetStadistics } from 'src/decorators/controllerDecorators/playerStadistics.decorator';
 
 @ApiTags("ESTADISTICAS DE JUGADORES")
 @Controller('player-stadistics')
 export class PlayerStadisticsController {
   constructor(private readonly playerStadisticsService: PlayerStadisticsService) {}
 
-  @ApiBearerAuth()
   @Get(":playerId")
-  @SwaggerGetStadistics()
-  @UseGuards(AuthGuard)
+  @CustomGetStadistics()
   getPlayerStadistics(@Param("playerId", ParseUUIDPipe) playerId: string){
     return this.playerStadisticsService.getPlayerStadistics(playerId)
   }
-  
 }
